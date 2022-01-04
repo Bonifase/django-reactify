e49f25115124dee15dd6ab44e4f248211455ba80
Bonifase Orwa <orwabonifase@gmail.com>
2022-01-04 19:55:46 +0300

    add management command

--- reactify/webpack.config.js
+++ reactify/webpack.config.js
@@ -1,42 +1,15 @@
 const path = require("path");
 const webpack = require("webpack");
-const { styles } = require( '@ckeditor/ckeditor5-dev-utils' );
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "./static/writing"),
+    path: path.resolve(__dirname, "./static/reactify"),
     filename: "[name].js",
   },
   module: {
     rules: [
-      {
-        test: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
-        use: [ 'raw-loader' ]
-      },
-      {
-        test: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/,
-        use: [
-            {
-                loader: 'style-loader',
-                options: {
-                    injectType: 'singletonStyleTag',
-                    attributes: {
-                        'data-cke': true
-                    }
-                }
-            },
-            {
-                loader: 'postcss-loader',
-                options: styles.getPostCssConfig( {
-                    themeImporter: {
-                        themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
-                    },
-                    minify: true
-                } )
-            }
-        ]
-      },
+
       {
         test: /\.js$/,
         exclude: /node_modules/,
