c3298a754cd7c12f023956aa8d69d751939f0abb
Bonifase Orwa <orwabonifase@gmail.com>
2022-01-08 16:51:53 +0300

    update material ui

--- reactify/react/components/App.js
+++ reactify/react/components/App.js
@@ -3,8 +3,6 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
-import ProTip from './ProTip';
-
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
